dd4e75a3643cc9f4e1713e8c76057f7c3b51f0ad
wongcw <rubygreat@msn.com>
2024-07-19 17:07:35 +0800

    fix: addfile

--- postgres.conf.sh
+++
@@ -1,5 +0,0 @@
-echo "cron.host = ''" >> /var/lib/postgresql/data/pg_hba.conf
-echo "cron.database_name = 'postgres'" >> /var/lib/postgresql/data/postgresql.conf
-echo "cron.timezone = 'PRC'" >> /var/lib/postgresql/data/postgresql.conf
-
-su postgres -c 'psql -c "CREATE EXTENSION IF NOT EXISTS pg_cron;"'
