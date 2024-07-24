375807969f08e9a1f98c41f7867a51eb9f6fb9f0
wongcw <rubygreat@msn.com>
2024-07-24 15:00:04 +0800

    add: add timezone in conf

--- initdb-extensions.sh
+++ initdb-extensions.sh
@@ -7,6 +7,7 @@ set -u
 echo "shared_preload_libraries = 'pg_stat_monitor,pg_cron,pg_uuidv7'" >> /var/lib/postgresql/data/postgresql.conf
 echo "cron.database_name = 'postgres'" >> /var/lib/postgresql/data/postgresql.conf
 echo "cron.timezone = 'PRC'" >> /var/lib/postgresql/data/postgresql.conf
+echo "timezone = 'Asia/Shanghai'" >> /var/lib/postgresql/data/postgresql.conf
 
 "${psql[@]}" --dbname=postgres <<-'EOSQL'
 CREATE EXTENSION IF NOT EXISTS pg_stat_monitor;
